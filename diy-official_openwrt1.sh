15b6c7082684bec1af72c3162615c482d3bfee2e
YingziWo <104121906+YingziWo@users.noreply.github.com>
2024-09-13 13:30:02 +0800

    Update diy-official_openwrt1.sh

--- diy-official_openwrt1.sh
+++ diy-official_openwrt1.sh
@@ -116,7 +116,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages hysteria
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages ipt2socks
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages microsocks
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages naiveproxy
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages pdnsd-alt
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages pdnsd-alt
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocks-rust
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages simple-obfs
@@ -128,7 +128,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages trojan-go
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages trojan-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages tuic-client
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-core
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-geodata
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-geodata
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2ray-plugin
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages xray-core
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages xray-plugin
