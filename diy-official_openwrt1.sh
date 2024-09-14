0fc129c4077f4deb737d6ee1e3aca9a5c9596886
YingziWo <104121906+YingziWo@users.noreply.github.com>
2024-09-14 12:40:16 +0800

    Update diy-official_openwrt1.sh

--- diy-official_openwrt1.sh
+++ diy-official_openwrt1.sh
@@ -189,6 +189,6 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 
-# 替换go版本到1.22.x
+# 替换go版本到1.23.x
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
