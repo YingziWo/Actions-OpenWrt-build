eb4a57d954ded1e77111677869a169615ed079ab
YingziWo <104121906+YingziWo@users.noreply.github.com>
2024-09-20 15:19:44 +0800

    Update diy-official_openwrt1.sh

--- diy-official_openwrt1.sh
+++ diy-official_openwrt1.sh
@@ -71,7 +71,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adg
 
 #科学上网
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-passwall
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ssr-plus
 #更换插件名称
 #sed -i 's/ShadowSocksR Plus+/软件插件/g' feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
