69b81877c48c83bb48087a0737292f09fbe18c39
YingziWo <104121906+YingziWo@users.noreply.github.com>
2024-10-22 11:17:08 +0800

    Update diy-ledeSL-part1.sh

--- diy-ledeSL-part1.sh
+++ diy-ledeSL-part1.sh
@@ -57,7 +57,7 @@ rm -rf feeds/luci/applications/luci-app-netdata
 #luci-theme-argone
 ##git_sparse_clone main https://github.com/kenzok8/small-package luci-theme-argone                     # argone for 18.06
 ##git_sparse_clone main https://github.com/kenzok8/small-package luci-app-argone-config
-git_sparse_clone master https://github.com/jerrykuku/luci-theme-argon luci-theme-argon                 # argon适应于23.05，这是原作者的链接
+git_sparse_clone master https://github.com/jerrykuku/luci-theme-argon luci-theme-argon                 # argon适应于23.05，这是原作者的链接 。同时支持改master --》 18.06版本 
 git_sparse_clone master https://github.com/jerrykuku/luci-app-argon-config luci-app-argon-config
 
 
