08de5fe70f85eddbefa8092fb0084bae442b6728
YingziWo <104121906+YingziWo@users.noreply.github.com>
2024-10-29 12:18:13 +0800

    Update diy-ledejj-part2.sh

--- diy-ledejj-part2.sh
+++ diy-ledejj-part2.sh
@@ -15,16 +15,16 @@ pwd
 ls
 
 # Modify default IP   #新版lede master原码仅这条是更改不了缺省ip地址，需要更改缺省的flash.js共同作用来实现
-sed -i 's/192.168.1.1/192.168.4.100/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.4.100/g' package/base-files/files/bin/config_generate
 
 #修改openwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.4.10/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+#sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.4.10/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 
 #添加项目地址
 #sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github项目%><\/td><td><a href="https:\/\/github.com\/lmxslpc\/OpenWrt-Build-System" target="_blank">云编译系统<\/a><\/td><\/tr>' ./package/lean/autocore/files/x86/index.htm
 # sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github项目%><\/td><td><a href="https:\/\/github.com\/YingziWo\/OpenWrt-Build-System" target="_blank">云编译系统<\/a><\/td><\/tr>' ./package/lean/autocore/files/x86/index.htm
 #sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github项目%><\/td><td><a href="https:\/\/github.com\/YingziWo\/Actions-OpenWrt-build" target="_blank">云编译系统<\/a><\/td><\/tr>' ./package/lean/autocore/files/x86/index.htm
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github Project%><\/td><td><a href="https:\/\/github.com\/YingziWo\/Actions-OpenWrt-build" target="_blank">Actions-Build-System<\/a><\/td><\/tr>' ./package/lean/autocore/files/x86/index.htm
+#sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a <tr><td width="33%"><%:Github Project%><\/td><td><a href="https:\/\/github.com\/YingziWo\/Actions-OpenWrt-build" target="_blank">Actions-Build-System<\/a><\/td><\/tr>' ./package/lean/autocore/files/x86/index.htm
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
