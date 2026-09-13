9a3d19122c45ea8219fe1e908352c86502fb38aa
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-09-13 13:09:41 +0800

    Change branch from master to main for git_sparse_clone

--- diy-ledejj-part1.sh
+++ diy-ledejj-part1.sh
@@ -162,9 +162,9 @@ git_sparse_clone main https://github.com/kenzok8/small-package luci-app-autotime
 ##git_sparse_clone master https://github.com/kiddin9/openwrt-packages redsocks2
 ##git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadow-tls
 ##git_sparse_clone master https://github.com/kiddin9/openwrt-packages lua-maxminddb
-git_sparse_clone master https://github.com/kenzok8/small lua-neturl                            #kenzok8 的small-package中无此插件，在small中 # 核查有260909
-git_sparse_clone master https://github.com/kenzok8/small redsocks2                             #kenzok8 的small-package中无此插件，在small中 # 核查有260909
-git_sparse_clone master https://github.com/kenzok8/small shadow-tls                            #kenzok8 的small-package中无此插件，在small中 # 核查有260909
+git_sparse_clone main https://github.com/kenzok8/small lua-neturl                            #kenzok8 的small-package中无此插件，在small中 # 核查有260909
+git_sparse_clone main https://github.com/kenzok8/small redsocks2                             #kenzok8 的small-package中无此插件，在small中 # 核查有260909
+git_sparse_clone main https://github.com/kenzok8/small shadow-tls                            #kenzok8 的small-package中无此插件，在small中 # 核查有260909
 git_sparse_clone main https://github.com/kenzok8/small-package lua-maxminddb                                                               # 核查有260909
 
 ##########################################其他设置##########################################
