746c152f50b781b77a42fb0c047fb310c7e6e272
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-09-13 19:47:18 +0800

    Add wall package source to feeds.conf.default

--- diy-ledejj-part1.sh
+++ diy-ledejj-part1.sh
@@ -23,6 +23,7 @@ ls
 sed -i "/helloworld/d" "feeds.conf.default"
 sed -i "1i src-git kenzo https://github.com/kenzok8/openwrt-packages" "feeds.conf.default"
 sed -i "2i src-git small https://github.com/kenzok8/small" "feeds.conf.default"
+sed -i "3i src-git wall https://github.com/kenzok8/wall" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
