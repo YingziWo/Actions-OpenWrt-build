0d72339559c6c05ca635cb8dd779b197b02dd48d
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-09-17 20:47:28 +0800

    增加绝对目录

--- diy-official-Wrt-from-Ledejj260909-2.sh
+++ diy-official-Wrt-from-Ledejj260909-2.sh
@@ -3,24 +3,24 @@
 #!/bin/bash
 set -e
 
-
+cd /home/runner/work/Actions-OpenWrt-build/Actions-OpenWrt-build/openwrt
 CONFIG_FILE2="package/base-files/files/bin/config_generate"
 
 # -------- 修改基础参数 --------
 
-sed -i 's/{ipaddr:-"192\.168\..*"}/{ipaddr:-"192.168.4.10"}/g' "$CONFIG_FILE"
-sed -i "s/hostname='OpenWrt'/hostname='OfficialWrt'/g" "$CONFIG_FILE"
-sed -i "s/timezone='UTC'/timezone='CST-8'/g" "$CONFIG_FILE"
+sed -i 's/{ipaddr:-"192\.168\..*"}/{ipaddr:-"192.168.4.10"}/g' "$CONFIG_FILE2"
+sed -i "s/hostname='OpenWrt'/hostname='OfficialWrt'/g" "$CONFIG_FILE2"
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" "$CONFIG_FILE2"
 
-if grep -Eq '^[[:space:]]*set system\.@system\[-1\]\.zonename=' "$CONFIG_FILE"; then
+if grep -Eq '^[[:space:]]*set system\.@system\[-1\]\.zonename=' "$CONFIG_FILE2"; then
     sed -i \
         "s|^\([[:space:]]*set system\.@system\[-1\]\.zonename=\).*|\1'Asia/Shanghai'|" \
-        "$CONFIG_FILE"
+        "$CONFIG_FILE2"
 else
     sed -i \
         "/set system\.@system\[-1\]\.timezone='CST-8'/a\\
         set system.@system[-1].zonename='Asia/Shanghai'" \
-        "$CONFIG_FILE"
+        "$CONFIG_FILE2"
 fi
 
 
