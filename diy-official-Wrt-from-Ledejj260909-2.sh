c62ae3af72360d97bc566ae755f04f961e45fe71
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-09-18 20:01:06 +0800

    Update diy-official-Wrt-from-Ledejj260909-2.sh

--- diy-official-Wrt-from-Ledejj260909-2.sh
+++ diy-official-Wrt-from-Ledejj260909-2.sh
@@ -50,8 +50,9 @@ EOT
 
 sed -i "s|BUILD_DATE|$BUILD_DATE|g" banner
 cd /home/runner/work/Actions-OpenWrt-build/Actions-OpenWrt-build/openwrt
-cp --backup=numbered version version.bak
-sed -i "s|-.*|-Firmware Is Built By YzW|g" version
+cp --backup=numbered version version.bak33
+#sed -i "1s|$ |-Firmware Is Built By YzW|g" version
+sed -i "2i|*|-Firmware Is Built By YzW|g" version
 
 
 echo "✅ Custom banner has been set."
