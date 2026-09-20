d14cd4ad3f6b212b1bc3d94868afd36885916c54
YingziWo <104121906+YingziWo@users.noreply.github.com>
2026-09-21 00:05:54 +0800

    Update diy-official-Wrt-from-Ledejj260909-2.sh

--- diy-official-Wrt-from-Ledejj260909-2.sh
+++ diy-official-Wrt-from-Ledejj260909-2.sh
@@ -50,14 +50,90 @@ EOT
 
 sed -i "s|BUILD_DATE|$BUILD_DATE|g" banner
 
-# 固件显示面板中增加版本编译地址url
+# 固件显示面板中增加版本编译地址url,这段代码小心调整，不知到用途勿随意调整，易造成编译失败
 cd /home/runner/work/Actions-OpenWrt-build/Actions-OpenWrt-build/openwrt/package/feeds/luci/luci-theme-bootstrap/ucode/template/themes/bootstrap
-sed -i 's|{"{{ entityencode(version.disturl ?? ''#'', true) }}"}/{"https://github.com/YingziWo/Actions-OpenWrt-build/releases"}/g' footer.ut
-sed -i 's|{"{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})"}/{"{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }}) Firmware Is Built By YzW"}/g' footer.ut
+#sed -i 's|{"{{ entityencode(version.disturl ?? ''#'', true) }}"}/{"https://github.com/YingziWo/Actions-OpenWrt-build/releases"}/g' footer.ut
+#sed -i 's|{"{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})"}/{"{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }}) Firmware Is Built By YzW"}/g' footer.ut
+rm -rf footer.ut
+cat <<'EOT' > footer.ut
+		{% if (!blank_page): %}
+		</div>
+		<footer>
+			<span>
+				Powered by
+				<a href="https://github.com/openwrt/luci" target="_blank" rel="noreferrer">
+					{{ version.luciname }} ({{ version.luciversion }})</a>
+				/
+				<a href="https://github.com/YingziWo/Actions-OpenWrt-build/releases" target="_blank" rel="noreferrer">
+					{{ version.distname }} {{ version.distversion }} ({{ version.distrevision }}) Firmware Is Built By YzW</a>
+				{% if (lua_active): %}
+					/ {{ _('Lua compatibility mode active') }}
+				{% endif %}
+			</span>
+			<ul class="breadcrumb pull-right" id="modemenu" style="display:none"></ul>
+		</footer>
+		<script>L.require('menu-bootstrap')</script>
+		{% endif %}
+	</body>
+</html>
+
+EOT
+
+
 
 cd /home/runner/work/Actions-OpenWrt-build/Actions-OpenWrt-build/openwrt/package/yingziwo/luci-theme-argon/ucode/template/themes/argon
-sed -i 's|{"{{ version.distname }} {{ version.distversion }}-{{ version.distrevision }}"}/{"Version {{ version.distname }} {{ version.distversion }}-{{ version.distrevision }} Frimware Is Built By YzW"}/g' footer_login.ut
-sed -i 's|{"{{ version.disturl }}"}/{"https://github.com/YingziWo/Actions-OpenWrt-build/releases"}/g'  footer_login.ut
+#sed -i 's|{"{{ version.distname }} {{ version.distversion }}-{{ version.distrevision }}"}/{"Version {{ version.distname }} {{ version.distversion }}-{{ version.distrevision }} Frimware Is Built By YzW"}/g' footer_login.ut
+#sed -i 's|{"{{ version.disturl }}"}/{"https://github.com/YingziWo/Actions-OpenWrt-build/releases"}/g'  footer_login.ut
+rm -rf footer_login.ut
+cat <<'EOT' > footer_login.ut
+{#
+	Argon is a clean HTML5 theme for LuCI. It is based on luci-theme-material Argon Template
+
+	luci-theme-argon
+	Copyright 2020 Jerrykuku <jerrykuku@qq.com>
+
+	Have a bug? Please create an issue here on GitHub!
+	https://github.com/jerrykuku/luci-theme-argon/issues
+
+	luci-theme-material:
+	Copyright 2015 Lutty Yang <lutty@wcan.in>
+
+	Agron Theme
+	https://demos.creative-tim.com/argon-dashboard/index.html
+
+	Licensed to the public under the Apache License 2.0
+-#}
+
+</div>
+<!-- added style="text-wrap: auto" but on this page it is still not the best solution. I will take it to consideration next time :) -->
+<footer style="text-wrap: auto">
+	<div>
+		<a class="luci-link" href="https://github.com/openwrt/luci" target="_blank">Powered by {{ version.luciname }} ({{ version.luciversion }})</a>
+		<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">ArgonTheme {# vPKG_VERSION #}</a>
+		<a class="luci-link" href="https://github.com/YingziWo/Actions-OpenWrt-build/releases" target="_blank">Version {{ version.distname }} {{ version.distversion }}-{{ version.distrevision }} Frimware Is Built By YzW</a>
+	</div>
+</footer>
+</div>
+</div>
+<script>
+	// thanks for Jo-Philipp Wich <jow@openwrt.org>
+	var luciLocation = {{ ctx.path }};
+	var winHeight = window.innerHeight;
+	window.addEventListener('resize', function () {
+		var winWidth = window.innerWidth;
+		if(winWidth < 600){
+			var newHeight = window.innerHeight;
+			var keyboradHeight = newHeight - winHeight;
+			var ftcElement = document.querySelector(".ftc");
+			if (ftcElement) {
+				ftcElement.style.bottom = (keyboradHeight + 30) + "px";
+			}
+		}
+	});
+</script>
+</body>
+</html>
+EOT
 
 #cp --backup=numbered version version.bak33
 #sed -i "1s|-.*|-Firmware Is Built By YzW|g" version #替换-后内容
